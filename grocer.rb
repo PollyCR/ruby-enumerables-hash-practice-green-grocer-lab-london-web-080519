c4e5f6599a435d1c8ab0d56af07c673f42a5ef41
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-18 15:46:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,13 +37,13 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  cart.keys.each do |item| 
+  cart.keys.each do |item|
     if cart[item][:clearance]
-      cart[item][:price] = (cart[item][:price] * 0.8).float(2)
+      cart[item][:price] = (cart[item][:price]*0.80).round(2)
+    end
+  end
+  cart
 end
-end 
-cart 
-end 
 
 
 def checkout(cart)
